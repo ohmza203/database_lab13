0994a77920702ea54d9056edd9fd3c6e6858d0d3
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 16:01:48 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-<title>Database</title>
-</head>
-<body>
-<h1><center><a href = "show.php">Show Data</a></center></h1>
-<h1><center><a href = "form.php">Add Data</a></center></h1>
-</body>
-</html>
