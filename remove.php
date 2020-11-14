c54d61d09c6662ce3bcfaba08a56d0c66f8051bb
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 17:22:42 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -13,7 +13,7 @@ $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = "DELETE FROM guestbook WHERE ('$name', '$comment', '$link')";
+$sql = "DELETE FROM guestbook (Name , Comment , Link) WHERE ('$name', '$comment', '$link')";
 
 
 if (mysqli_query($conn, $sql)) {
