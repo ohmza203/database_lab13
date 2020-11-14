e9fff300ad42753875854feb2af132677197e9eb
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 17:13:15 +0700

    Create remove.php

---
+++ remove.php
@@ -0,0 +1,34 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'ohmbase.mysql.database.azure.com', 'ohmzasa203@ohmbase', 'Ohmmie203', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "DELETE FROM guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
+<html>
+    <head>
+    </head>
+    <body>
+        <a href = "show.php">Show Data</a>
+        <a href = "form.php">Add data</a>
+    </body>
+</html>
