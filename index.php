afd0c050c941bf9a3bb1d6f33f08b219172ab4d1
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 16:44:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,6 +33,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
+    <td><?php echo $Result['Action'];?><button type="button" class="btn btn-warning">เพิ่ม</button></td>
   </tr>
 <?php
 }
