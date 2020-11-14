442d787c1e27706a5c8997cdfa7470f0ad8b6f4e
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 15:40:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <title>Database</title>
 </head>
 <body>
-<h1><center><a herf = "show.php">Show Data</a></center></h1>
-<h1><center><a herf = "insert.php">Add Data</a></center></h1>
+<h1><center><a href = "show.php">Show Data</a></center></h1>
+<h1><center><a href = "insert.php">Add Data</a></center></h1>
 </body>
 </html>
