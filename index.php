d569033ca0777541eb8c9344874b528eb15d5afe
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 16:56:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,10 +20,10 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table class="table table-dark table-hover table table-bordered table table-hover">
   <tr>
-      <th width="150"> <div align="center"><h4>Name</h4></div></th>
-      <th width="350"> <div align="center"><h4>Comment </h4></div></th>
-      <th width="150"> <div align="center"><h4>Link </h4></div></th>
-      <th width="100"> <div align="center"><h4>Action </h4></div></th>
+      <th width="25%"> <div align="center"><h4>Name</h4></div></th>
+      <th width="30%"> <div align="center"><h4>Comment </h4></div></th>
+      <th width="25%"> <div align="center"><h4>Link </h4></div></th>
+      <th width="20%"> <div align="center"><h4>Action </h4></div></th>
     </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
