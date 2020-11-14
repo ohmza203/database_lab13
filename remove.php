cc13f1ef57705b0f8f99a4526ca64c7cd2b421a7
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 18:02:03 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$sql = "DELETE FROM guestbook WHERE SELECT * FROM guestbook WHERE name";
+$sql = "DELETE FROM guestbook WHERE name=SELECT * FROM guestbook WHERE name";
 
 
 if (mysqli_query($conn, $sql)) {
