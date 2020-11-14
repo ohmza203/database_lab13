a3fe33d9c63448ce7f7cebca103820b3260cc2c6
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 17:39:50 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$sql = "DELETE FROM guestbook";
+$sql = "DELETE FROM guestbook WHERE name='Test1'";
 
 
 if (mysqli_query($conn, $sql)) {
