6c99e9728ea6c0c1e2db64d655485f74353d1ff4
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 15:37:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Database</title>
+</head>
+<body>
+<h1><center><a herf = "show.php">Show Data</a></center></h1>
+<h1><center><a herf = "insert.php">Add Data</a></center></h1>
+</body>
+</html>
