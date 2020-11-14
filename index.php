09ce533e11f505d124fc939d619de1010bba1736
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 16:12:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table class="table table-dark table-hover">
   <tr>
-    <th width="100"> <div align="center">Name</div></th>
+    <th width="150"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Link </div></th>
     <th width="100"> <div align="center">Action </div></th>
