4c1ff8008166741ab3a7d149f9d838d71460c4a1
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-14 18:14:14 +0700

    Create edit.php

---
+++ edit.php
@@ -0,0 +1,33 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'ohmbase.mysql.database.azure.com', 'ohmzasa203@ohmbase', 'Ohmmie203', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+$link = $_POST['link'];
+
+
+$sql = "UPDATE guestbook SET name='$name', comment='$comment', link='$link'";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo"Record has been update";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
+<html>
+    <head>
+    </head>
+    <body>
+        <a href = "index.php">Continue</a>
+    </body>
+</html>
