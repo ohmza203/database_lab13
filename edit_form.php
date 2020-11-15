36aefe0aaf696d587f8b340f95a425fc8471dc82
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 02:33:19 +0700

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
+<?php
+$edit_id = $_REQUEST['edit_id']; 
+?>
 <html>
 <head>
 	<title>Comment Form</title>
 </head>
 <body>
-<?php$edit_id = $_REQUEST['edit_id']; ?>
   <form action = "edit.php?edit_id=<?php echo $edit_id; ?>" method = "post" id="CommentForm" >
     Name:<br>
     <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
