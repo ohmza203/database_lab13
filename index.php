b6089b93e282b5edb9a72d5fb7a512b634e5c949
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 01:31:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
-    <td><?php echo $Result['Action'];?><a href = "remove.php"><button type="button" class="btn btn-warning">Remove</button></a>   <a href = "edit_form.php"><button type="button" class="btn btn-warning">Edit</button></a></td>
+    <td><?php echo $Result['Action'];?><a href = "remove.php?delete_id=<?php echo $Result['ID']; ?>"><button type="button" class="btn btn-warning">Remove</button></a>   <a href = "edit_form.php"><button type="button" class="btn btn-warning">Edit</button></a></td>
   </tr>
 <?php
 }
