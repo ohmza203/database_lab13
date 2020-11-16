5baf7bab24e247becff4af9d664398e7ca04ebeb
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 22:44:51 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,33 +1,41 @@
-<?php
-
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'ohmbase.mysql.database.azure.com', 'ohmzasa203@ohmbase', 'Ohmmie203', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
-
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
-
-
-$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
-
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-mysqli_close($conn);
-?>
 <html>
     <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
-    <body>
-        <a href = "index.php">Continue</a>
+    <body style="background-color:#303030;">
+        <?php
+        
+        $conn = mysqli_init();
+        mysqli_real_connect($conn, 'ohmbase.mysql.database.azure.com', 'ohmzasa203@ohmbase', 'Ohmmie203', 'itflab', 3306);
+        if (mysqli_connect_errno($conn))
+        {
+            die('Failed to connect to MySQL: '.mysqli_connect_error());
+        }
+
+
+        $name = $_POST['name'];
+        $comment = $_POST['comment'];
+        $link = $_POST['link'];
+
+
+        $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
+
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<h2 style='text-align:center; color:white;'>New record created successfully</h2>";
+          } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+          }
+
+        mysqli_close($conn);
+        ?>
+        
+        
+        <center><a href = "index.php"><button type="button" class="btn btn-info">Continue</button></a></center>
     </body>
 </html>
