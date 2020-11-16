39c4139cec435182baeea16f6bd8d14ebe4d6603
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 22:11:55 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -21,8 +21,14 @@ mysqli_close($conn);
 ?>
 <html>
     <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <td><a href = "index.php"><button type="button" class="btn btn-info">Continue</button></a></td>
+        <center><a href = "index.php"><button type="button" class="btn btn-info">Continue</button></a></center>
     </body>
 </html>
