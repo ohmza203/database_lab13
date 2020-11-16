2accd76e417ad6c54b7953eab4c9d08b2390ed26
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 22:57:37 +0700

    Update form.php

--- form.php
+++ form.php
@@ -2,14 +2,19 @@
 <html>
 <head>
 	<title>ITF lab</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body style="background-color:#303030;">
 	<form action = "insert.php" method = "post" id="CommentForm" >
-		Name:<br>
+		<h5 style='color:white;'>Name:</h5><br>
 		<input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
-		Comment:<br>
+		<h5 style='color:white;'>Comment:</h5><br>
 		<textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
-		Link:<br>
+		<h5 style='color:white;'>Link:</h5><br>
 		<input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
 		<input type="submit" id="commentBtn">
 	</form> 
