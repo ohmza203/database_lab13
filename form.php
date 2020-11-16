6613c419f7cfe42891962af7649916ba98de988c
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 22:53:35 +0700

    Update form.php

--- form.php
+++ form.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Comment Form</title>
+	<title>ITF lab</title>
 </head>
-<body>
-  <form action = "insert.php" method = "post" id="CommentForm" >
-    Name:<br>
-    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
-    Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
-    Link:<br>
-    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
-    <input type="submit" id="commentBtn">
-  </form> 
+<body style="background-color:#303030;">
+	<form action = "insert.php" method = "post" id="CommentForm" >
+		Name:<br>
+		<input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+		Comment:<br>
+		<textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+		Link:<br>
+		<input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
+		<input type="submit" id="commentBtn">
+	</form> 
 </body>
 </html>
