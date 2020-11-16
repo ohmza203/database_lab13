45da757e30e6d5cabbdfd62bbcca8eb5eadb0e20
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 23:27:33 +0700

    Update form.php

--- form.php
+++ form.php
@@ -10,20 +10,20 @@
 </head>
 <body style="background-color:#303030;">
 	<form class="form-horizontal" action = "insert.php" method = "post" id="CommentForm" >
-		<div class="form-group">
-			<label class="control-label col-sm-2" for="name"><h3 style='color:white;'>Name:</h3></label>
+		<div class="form-group form-group-sm">
+			<label class="col-sm-2 control-label" for="name"><h3 style='color:white;'>Name:</h3></label>
 		    <div class="col-sm-10">
 		      <br><input type="text" class="form-control" name = "name" id="idName" placeholder="Enter Name">
 		    </div>
 		 </div>
-		 <div class="form-group">
-		    <label class="control-label col-sm-2" for="comment"><h3 style='color:white;'>Comment:</h3></label>
+		 <div class="form-group form-group-lg">
+		    <label class="col-sm-2 control-label" for="comment"><h3 style='color:white;'>Comment:</h3></label>
 		    <div class="col-sm-10">
 		      <textarea rows="10" col="20" class="form-control" name = "comment" id="idComment" placeholder="Enter Comment"></textarea>
 		    </div>
 		  </div>
-		<div class="form-group">
-		    <label class="control-label col-sm-2" for="link"><h3 style='color:white;'>Link:</h3></label>
+		<div class="form-group form-group-sm">
+		    <label class="col-sm-2 control-label" for="link"><h3 style='color:white;'>Link:</h3></label>
 		    <div class="col-sm-10">
 		      <input type="text" class="form-control" name = "link" id="idLink" placeholder="Enter Link">
 		    </div>
