5a3d9b60ab9f724ce4cf1f11b9ecbe2b87293e26
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 23:37:07 +0700

    Update edit_form.php

--- edit_form.php
+++ edit_form.php
@@ -4,17 +4,38 @@ $edit_id = $_REQUEST['edit_id'];
 ?>
 <html>
 <head>
-	<title>Comment Form</title>
+	<title>ITF lab</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-  <form action = "edit.php?edit_id=<?php echo $edit_id; ?>" method = "post" id="CommentForm" >
-    Name:<br>
-    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
-    Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
-    Link:<br>
-    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
-    <input type="submit" id="commentBtn">
-  </form> 
+ <body style="background-color:#303030;">
+    <h2 style='text-align:center; color:white;'>Edit form</h2>
+    <div class="container">
+	<form class="form-horizontal" action = "insert.php" method = "post" id="CommentForm" >
+		<div class="form-group form-group-sm">
+			<label class="col-sm-2 control-label" for="name"><h3 style='color:white;'>Name:</h3></label>
+		    <div class="col-sm-10">
+		      <br><input type="text" class="form-control" name = "name" id="name" placeholder="Enter Name">
+		    </div>
+		 </div>
+		 <div class="form-group form-group-lg">
+		    <label class="col-sm-2 control-label" for="comment"><h3 style='color:white;'>Comment:</h3></label>
+		    <div class="col-sm-10">
+		      <textarea rows="10" col="20" class="form-control" name = "comment" id="comment" placeholder="Enter Comment"></textarea>
+		    </div>
+		  </div>
+		<div class="form-group form-group-sm">
+		    <label class="col-sm-2 control-label" for="link"><h3 style='color:white;'>Link:</h3></label>
+		    <div class="col-sm-10">
+		      <br><input type="text" class="form-control" name = "link" id="link" placeholder="Enter Link">
+		    </div>
+		</div>
+		<center><button type="submit" class="btn btn-success">Submit</button></center>
+	</form>
+	</div>
 </body>
 </html>
