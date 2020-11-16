f61d6f830026baa82a641c78ffe0eaf7ba1882cd
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-16 22:27:59 +0700

    Update remove.php

--- remove.php
+++ remove.php
@@ -1,24 +1,3 @@
-<?php
-
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'ohmbase.mysql.database.azure.com', 'ohmzasa203@ohmbase', 'Ohmmie203', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-
-$id = $_REQUEST['delete_id'];
-$sql = "DELETE FROM guestbook WHERE id='$id'";
-
-
-if (mysqli_query($conn, $sql)) {
-    echo "<center><h2>Record has been removed</h2></center>";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-mysqli_close($conn);
-?>
 <html>
     <head>
         <title>Bootstrap Example</title>
@@ -29,6 +8,27 @@ mysqli_close($conn);
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
     <body>
+        <?php
+
+            $conn = mysqli_init();
+            mysqli_real_connect($conn, 'ohmbase.mysql.database.azure.com', 'ohmzasa203@ohmbase', 'Ohmmie203', 'itflab', 3306);
+            if (mysqli_connect_errno($conn))
+            {
+                die('Failed to connect to MySQL: '.mysqli_connect_error());
+            }
+
+            $id = $_REQUEST['delete_id'];
+            $sql = "DELETE FROM guestbook WHERE id='$id'";
+
+
+            if (mysqli_query($conn, $sql)) {
+                echo "<center><h2>Record has been removed</h2></center>";
+            } else {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            }
+  
+            mysqli_close($conn);
+        ?>
         <center><a href = "index.php"><button type="button" class="btn btn-info">Continue</button></a></center>
     </body>
 </html>
