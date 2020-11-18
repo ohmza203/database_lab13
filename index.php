ef7c9a295c2801c86011cb1ae9052635396b2cd6
ohmza203 <73680009+ohmza203@users.noreply.github.com>
2020-11-18 12:46:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" />
     </head>
     <body style="background-color:#303030;">
         <?php
@@ -18,7 +22,7 @@
             }
             $res = mysqli_query($conn, 'SELECT * FROM guestbook');
         ?>
-        <table class="table table-dark table-hover table table-bordered table table-hover">
+        <table id="myTable" class="display table table-dark table-hover table table-bordered table table-hover">
             <thead class="thead-dark">
                 <tr>
                     <th width="25%"> <div align="center"><h4>Name</h4></div></th>
@@ -42,6 +46,11 @@
             }
         ?>
         </table>
+        <script>
+        $(document).ready(function () {
+            $("#myTable").DataTable();
+        });
+        </script>
         <?php
             mysqli_close($conn);
         ?>
